1b3515c1f89eacbdb845ddca8092de376a778c00
WangYG <wangyonggang@ctyun.cn>
2016-03-09 19:12:15 +0800

    已完成

--- front/app/index.jsx
+++ front/app/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Input } from 'react-bootstrap';
+import { Button, Input, Row } from 'react-bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,6 +7,7 @@ const ThisPage = React.createClass({
 	getInitialState() {
     return {
       signBtnDisabled: true,
+      showText : "请注意, 此项目是React和Flask结合的练习产品, 严禁用于非工作区域签到, 敬业光荣!",
       form:{
       	iccid:"",
       	phone:"",
@@ -19,15 +20,50 @@ const ThisPage = React.createClass({
   		this.setState({signBtnDisabled: false});
   	}
   },
+  startSign (){
+    this.setState({
+      signBtnDisabled: true,
+      showText: "正在签到, 请稍等..."
+    });
+    jQuery.ajax({
+      url:"/api/sign",
+      method:"POST",
+      data: JSON.stringify({
+        "iccid":this.refs.iccid.getValue(),
+        "phone":this.refs.phone.getValue(),
+        "password":this.refs.password.getValue()
+      }),
+      contentType:"application/json", 
+      success: data => {
+        this.setState({
+          signBtnDisabled: false,
+          showText: data
+        });
+      },
+      error: (xml,e) => {
+        this.setState({
+          signBtnDisabled: false,
+          showText: "发生如下错误: "+e
+        });
+      }
+    })
+  },
 	render() {
 		return(
 			<div>
-				<form className="form-horizontal col-xs-12 col-md-6 col-md-offset-3	">
-					<Input type="text" label="iccid" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="iccid"/>
-					<Input type="text" label="手机号" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="phone" />
-					<Input type="password" label="密码" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="password" />
-					<Button bsStyle="primary" bsSize="block" disabled={this.state.signBtnDisabled} className="col-xs-offset-4 col-xs-4">Bugly签到</Button>
-				</form>
+        <Row>
+          <p className=" col-xs-12 col-md-6 col-md-offset-3 ">
+            {this.state.showText}
+          </p>
+        </Row>
+        <Row>
+  				<form className="form-horizontal col-xs-12 col-md-6 col-md-offset-3	">
+  					<Input type="text" label="iccid" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="iccid"/>
+  					<Input type="text" label="手机号" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="phone" />
+  					<Input type="password" label="密码" labelClassName="col-xs-2" wrapperClassName="col-xs-10" onChange={this.handleInput} ref="password" />
+  					<Button bsStyle="primary" bsSize="lg" disabled={this.state.signBtnDisabled} className="col-xs-offset-4 col-xs-4" onClick={this.startSign}>Bugly签到</Button>
+  				</form>
+        </Row>
 			</div>
 		);
 	}
--- server.py
+++ server.py
@@ -1,9 +1,88 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+import urllib2,urllib
+import cookielib
+import json
+
+import sys
+reload(sys)
+sys. setdefaultencoding('utf8')
 app = Flask(__name__, static_folder='front')
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html', debugMode=True)
 
+@app.route('/api/sign',  methods=['POST'])
+def sign():
+    form = request.get_json()
+    result = signOnce(form["iccid"],form["phone"],form["password"])
+    return result
+
+
+
+def signOnce(iccid, phone, password):
+    if iccid=="1" and phone=="1" and password=="1":
+        iccid = "89860315840104315205"
+        phone= "17710432234"
+        password = "yun123456"
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+
+    # 登陆客户端
+    loginInfo = json.dumps({"iccid":iccid,"loginName":phone,"password":password})
+    req = urllib2.Request('http://www.yijitongoa.com:9090/yjtoa/s/clientlogin')
+    req.add_header('User-Agent','yjt-oa')
+    req.add_header('clientVersion','android_10409')
+    req.add_header('Cookie2','$Version=1')
+    req.add_header('apiVer','1')
+    req.add_header('Content-Type','application/json; charset=utf-8')
+    req.add_data(loginInfo)
+    response = urllib2.urlopen(req)
+
+    response = json.loads(response.read())
+    if not response.get("payload"):
+        return "签到失败!"+str(response)
+    userId = response["payload"][0]["userId"]
+    print "客户端已登录成功, 用户id %s" %userId
+
+
+    # 登陆系统
+    loginInfo = response["payload"][0]
+    loginInfo["password"] = password
+    loginInfo["iccid"] = iccid
+    req = urllib2.Request('http://www.yijitongoa.com:9090/yjtoa/s/reallogin')
+    req.add_header('User-Agent','yjt-oa')
+    req.add_header('clientVersion','android_10409')
+    req.add_header('Cookie2','$Version=1')
+    req.add_header('apiVer','1')
+    req.add_header('Content-Type','application/json; charset=utf-8')
+    req.add_data(json.dumps(loginInfo))
+    response = urllib2.urlopen(req)
+
+    response = json.loads(response.read())
+    if response["code"] == 0:
+        print "系统已登录成功"
+    else:
+        print "系统登录失败"
+        return "系统登陆失败!"+ str(response)
+
+
+    #签到
+    signInfo = {"descColor":0,"iccId":iccid,"id":0,"positionData":"39.962446,116.229728","positionDescription":"中国北京市海淀区杏石口路99号","resultColor":0,"signResult":0,"type":"VISIT","userId":userId}
+    req = urllib2.Request('http://www.yijitongoa.com:9090/yjtoa/s/signins/attendances')
+    req.add_header('User-Agent','yjt-oa')
+    req.add_header('clientVersion','android_10409')
+    req.add_header('Cookie2','$Version=1')
+    req.add_header('apiVer','1')
+    req.add_header('Content-Type','application/json; charset=utf-8')
+    req.add_data(json.dumps(signInfo))
+    response = urllib2.urlopen(req)
+    response = response.read()
+    print "签到完毕, 结果如下"
+    print response
+    return str(response)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
