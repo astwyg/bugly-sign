7f00d45ea67917834b9e9c514e679769cf48342e
WangYG <wangyonggang@ctyun.cn>
2017-01-13 14:31:43 +0800

    搞完订餐

--- dinnerOrder.py
+++ dinnerOrder.py
@@ -6,6 +6,22 @@ user_list = [
     {
         "username":"17710432234",
         "passwd":"tmyi5177163"
+    },
+    {
+        "username":"15031221287",
+        "passwd":"123123"
+    },
+    {
+        "username":"18201160619",
+        "passwd":"123456",
+    },
+    {
+        "username":"18210955909",
+        "passwd":"123456",
+    },
+    {
+        "username":"18518488066",
+        "passwd":"guojie"
     }
 ]
 
@@ -16,20 +32,24 @@ def make_order(user):
     r = s.post("http://life.ctyun.com.cn/ajax/getTodayMeal", json={"pageNo":1,"pageSize":6,"query":{}})
     meal_id = None
     for meal in r.json().get("returnObj").get("result"):
-        if "西山赢府" in meal["mealName"]:
+        if u"西山赢府" in meal["mealName"]:
             meal_id = meal["id"]
             break
-    r = s.post()
+    r = s.post("http://life.ctyun.com.cn/ajax/takeOrder", json={"addressId":2, "id":meal_id})
+    return r.json().get("returnObj").get("msg")
 
 
 
 def make_orders(users):
     for user in users:
         print("开始为{}订餐".format(user["username"]))
-        print(make_order(user))
+        try:
+            print(make_order(user))
+        except Exception, e:
+            print("发生错误:"+str(e))
+        print("="*10)
 
 
 if __name__ == "__main__":
-    global user_list
     make_orders(user_list)
 
